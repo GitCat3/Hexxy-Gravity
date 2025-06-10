8461af2ab8dc40565e9a367b1a78ee31bbd6f4a4
GitCat3 <hulkbuster119@gmail.com>
2025-06-09 20:47:30 -0400

    fixed some good ol floating point math errors

--- src/main/java/me/gitcat3/hexxygravity/casting/patterns/spells/ChangeGravity.kt
+++ src/main/java/me/gitcat3/hexxygravity/casting/patterns/spells/ChangeGravity.kt
@@ -11,7 +11,7 @@ import at.petrak.hexcasting.api.misc.MediaConstants
 import gravity_changer.api.GravityChangerAPI
 import net.minecraft.entity.Entity
 import net.minecraft.util.math.Direction
-import net.minecraft.util.math.Vec3d
+import net.minecraft.util.math.Vec3i
 
 class ChangeGravity : SpellAction {
     override val argc = 2
@@ -19,14 +19,16 @@ class ChangeGravity : SpellAction {
         val target = args.getEntity(0)
         env.assertEntityInRange(target)
 
-        val direction = when (args.getVec3(1)) {
-            Vec3d(0.0, -1.0, 0.0) -> Direction.DOWN
-            Vec3d(0.0, 1.0, 0.0) -> Direction.UP
-            Vec3d(-1.0, 0.0, 0.0) -> Direction.WEST
-            Vec3d(1.0, 0.0, 0.0) -> Direction.EAST
-            Vec3d(0.0, 0.0, 1.0) -> Direction.SOUTH
-            Vec3d(0.0, 0.0, -1.0) -> Direction.NORTH
-            else -> throw MishapInvalidIota.Companion.of(args[1], 0, "axis_vector")
+        val unitvec = args.getVec3(1)
+        val unitvecint = Vec3i(unitvec.x.toInt(), unitvec.y.toInt(), unitvec.z.toInt())
+        val direction = when (unitvecint) {
+            Vec3i(0, -1, 0) -> Direction.DOWN
+            Vec3i(0, 1, 0) -> Direction.UP
+            Vec3i(-1, 0, 0) -> Direction.WEST
+            Vec3i(1, 0, 0) -> Direction.EAST
+            Vec3i(0, 0, 1) -> Direction.SOUTH
+            Vec3i(0, 0, -1) -> Direction.NORTH
+            else -> throw MishapInvalidIota.of(args[1], 0, "axis_vector")
         }
 
         return SpellAction.Result(Spell(target, direction), MediaConstants.CRYSTAL_UNIT, listOf())
